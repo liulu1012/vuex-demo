2d14d16c48df850e49ae8c694b0a17b6256d37dd
liulu1012 <645911726@qq.com>
2017-08-08 10:10:13 +0000

    vue

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+  body {
+    margin: 0;
+  }
+  h1, h2, li {
+    margin: 0;
+  }
+  li {
+    list-style: none;
+  }
+  ul {
+    padding: 0;
+    margin: 0;
+  }
+</style>
